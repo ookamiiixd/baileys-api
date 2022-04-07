b33c8139166c959a10be3cec69c9772c767c129f
Tiago André <thiago_andre@live.com>
2022-04-07 10:25:19 -0300

    Update whatsapp.js

--- whatsapp.js
+++ whatsapp.js
@@ -254,7 +254,7 @@ const getChatList = (sessionId, isGroup = false) => {
 }
 
 const getGroupsWithParticipants = async (session) => {
-    const groups = await session.extractGroupMetadata()
+    const groups = await session.groupFetchAllParticipating()
     return groups
 }
 
