f1d2e076048f9b9d5eef660db5658eb407eb6dbe
_dev_goyal__ <65810521+dgbkn@users.noreply.github.com>
2022-06-04 07:06:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import { init, cleanup } from './whatsapp.js'
 
 const app = express()
 const host = process.env.HOST ?? '127.0.0.1'
-const port = parseInt(process.env.PORT ?? 8000)
+const port = parseInt(process.env.PORT ?? 3333)
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
