99854a4ef660a55c8ed3f653e41e1636fa0c67d4
_dev_goyal__ <65810521+dgbkn@users.noreply.github.com>
2022-06-04 07:03:39 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+import express from 'express'
+import nodeCleanup from 'node-cleanup'
+import routes from '../routes.js'
+import { init, cleanup } from '../whatsapp.js'
+
+const app = express()
+const host = process.env.HOST ?? '127.0.0.1'
+const port = parseInt(process.env.PORT ?? 8000)
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use('/', routes)
+
+app.listen(port, () => {
+    console.log(`Server is listening on http://${host}:${port}`)
+    init()
+})
+
+
+nodeCleanup(cleanup)
+
+export default app
