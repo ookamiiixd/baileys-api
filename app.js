b7ba3bbdeeb75ce7fb90827b6a008a9bc458cc98
Royhan <60024250+ookamiiixd@users.noreply.github.com>
2022-06-21 14:58:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,25 +6,24 @@ import { init, cleanup } from './whatsapp.js'
 import cors from 'cors'
 
 const app = express()
-app.use(cors())
 
-const host = process.env.HOST ?? '127.0.0.1'
-const onHeroku = process.env.HEROKU ?? false
+const host = process.env.HOST || undefined
 const port = parseInt(process.env.PORT ?? 8000)
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/', routes)
 
-if(onHeroku){
-    app.listen(port, () => {
+if(host){
+    app.listen(port, host, () => {
     init()
     console.log(`Server is listening on http://${host}:${port}`)
 })
 }else{
-app.listen(port, host, () => {
+app.listen(port, () => {
     init()
-    console.log(`Server is listening on http://${host}:${port}`)
+    console.log(`Server is listening on http://localhost:${port}`)
 })
 }
 
