57781ff3d3497876226d8db2887d84c94fa2a446
Ronen Filho <ronen.filho@gmail.com>
2022-08-13 04:22:13 +0000

    Fix:Get host

--- app.js
+++ app.js
@@ -4,10 +4,39 @@ import nodeCleanup from 'node-cleanup'
 import routes from './routes.js'
 import { init, cleanup } from './whatsapp.js'
 import cors from 'cors'
+import { networkInterfaces } from 'os'
 
 const app = express()
 
-const host = process.env.HOST || undefined
+const nets = networkInterfaces();
+const results = Object.create(null);
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+        if (net.family === familyV4Value && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
+
+// PARA PEGAR O IP DO HOST
+const ip_host = results["eth0"][0]
+console.log(ip_host);
+
+//TODO PARA TESTAR O IP
+app.get("/", function (req, res) {
+  console.log(req.socket.remoteAddress);
+  console.log(req.socket.localAddress);
+  res.send("your IP is: " + req.ip);
+});
+
+
+
+const host = ip_host || undefined
 const port = parseInt(process.env.PORT ?? 8000)
 
 app.use(cors())
