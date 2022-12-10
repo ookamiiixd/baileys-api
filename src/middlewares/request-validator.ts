3fb430b7a49316dd5aae4cdc49a886ab01780a45
Royhan <60024250+ookamiiixd@users.noreply.github.com>
2022-12-10 07:02:35 +0700

    missing return

--- src/middlewares/request-validator.ts
+++ src/middlewares/request-validator.ts
@@ -3,7 +3,7 @@ import { validationResult } from 'express-validator';
 
 const validate: RequestHandler = (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) res.status(400).json({ errors: errors.array() });
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
   next();
 };
 
