a265e14491027de2cfdb1c706e1a03c4304c7d90
luis pimenta <luis.pimenta.it@gmail.com>
2023-01-09 17:05:36 +0000

    add most common group functions

--- src/routes/groups.ts
+++ src/routes/groups.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { query } from 'express-validator';
+import { body, query } from 'express-validator';
 import * as controller from '../controllers/group';
 import requestValidator from '../middlewares/request-validator';
 import sessionValidator from '../middlewares/session-validator';
@@ -14,5 +14,54 @@ router.get(
 );
 router.get('/:jid', sessionValidator, controller.find);
 router.get('/:jid/photo', sessionValidator, controller.photo);
+router.get('/group/invitecode', sessionValidator, controller.inviteCode);
+router.get('/group/fetchallparticipating', sessionValidator, controller.groupFetchAllParticipating);
+router.post(
+  '/:jid/updatesubject',
+  body('subject').isString().notEmpty(),
+  requestValidator,
+  sessionValidator,
+  controller.updateSubject
+);
+router.post(
+  '/:jid/updatedescription',
+  body('description').isString().notEmpty(),
+  requestValidator,
+  sessionValidator,
+  controller.updateDescription
+);
+router.post(
+  '/:jid/updatesetting',
+  body('action')
+    .isString()
+    .notEmpty()
+    .isIn(['announcement', 'not_announcement', 'locked', 'unlocked']),
+  requestValidator,
+  sessionValidator,
+  controller.updateSetting
+);
+router.post(
+  '/:jid/updatepicture',
+  body('url').isString().notEmpty(),
+  requestValidator,
+  sessionValidator,
+  controller.updatePicture
+);
+router.post(
+  '/group/create',
+  body('name').isString().notEmpty(),
+  body('users').isArray(),
+  requestValidator,
+  sessionValidator,
+  controller.groupCreate
+);
+router.post(
+  '/:jid/participantsupdate',
+  body('users'),
+  body('action').isString().notEmpty().isIn(['add', 'remove', 'demote', 'promote']),
+  requestValidator,
+  sessionValidator,
+  controller.groupParticipantsUpdate
+);
 
 export default router;
